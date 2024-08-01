0e71d4f826e9d3e8af49e48dfeb706e7be3d4175
Sergei Mirzakhanov <mirzhn@gmail.com>
2024-08-01 12:26:11 +0300

    upgrade deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,7 @@
 PROJECT_DIR="/root/projects/board2gram"
 cd $PROJECT_DIR
 
-git pull
 source venv/bin/activate
 pip install -r requirements.txt
+alembic upgrade head
+sudo systemctl restart board2gram_bot.service
